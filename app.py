75b91f6cab69a2af9f307b785723820209bee0a7
faheemPakistani <69565922+faheemPakistani@users.noreply.github.com>
2020-12-24 16:08:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def image_resizing(image):
 
 def img_to_encoding(image_path, model):
 #     img1 = cv2.imread(image_path)
-    img = img1[..., ::-1]
+    img = image_path[..., ::-1]
     img = np.around(np.transpose(img, (2, 0, 1))/255.0, decimals=12)
     x_train = np.array([img])
     embedding = model.predict_on_batch(x_train)
